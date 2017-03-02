dd7a60e14a076b4a6408eb73fa33167a4e26a7ab
wulinan <wulinan_wln@126.com>
2017-03-02 22:10:55 +0800

    删除测试的app类

--- my/src/main/java/com/tos/module/apiprovider/ApiProviderMain.java
+++ my/src/main/java/com/tos/module/apiprovider/ApiProviderMain.java
@@ -2,10 +2,13 @@ package com.tos.module.apiprovider;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jettison.JettisonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main class.
@@ -13,33 +16,42 @@ import java.net.URI;
  */
 public class ApiProviderMain {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in test.test package
-        final ResourceConfig rc = new ResourceConfig().packages("com.tos.module.apiprovider");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
-
+    public static final URI BASE_URI = URI.create("http://localhost:8080/myapp/");
     /**
      * Main method.
      * @param args
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
+  
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+
+    
+    try {
+        System.out.println("JSON with JAXB Jersey Example App");
+
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, createApp(), false);
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                server.shutdownNow();
+            }
+        }));
+        server.start();
+
+        System.out.println(String.format("Application started.%nStop the application using CTRL+C"));
+
+        Thread.currentThread().join();
+    } catch (IOException | InterruptedException ex) {
+        Logger.getLogger(ApiProviderMain.class.getName()).log(Level.SEVERE, null, ex);
     }
+}
+
+public static ResourceConfig createApp() {
+    return new ResourceConfig()
+            .register(new JettisonFeature())
+            .packages("com.tos.module.apiprovider");
+}
 }
 
